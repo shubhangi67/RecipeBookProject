afc8725101df675e3bb7dd7004e03cbb7310de67
Shubhangi Suryavanshi <shubhi.191999@gmail.com>
2022-04-25 18:18:29 +0530

    Initial Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'RecipeBookProject';
+  loadedFeature = 'recipe';
+
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
+  }
 }
